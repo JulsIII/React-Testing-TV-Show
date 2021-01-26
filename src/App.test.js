f57e69feb4bbc6d2eb089b636dda308f61bea233
Your Name <you@example.com>
2021-01-25 19:19:57 -0600

    4 - added 1st tests, MVP met, but need more tests

---
+++ src/App.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+// import {fetchMissions as mockFetchMissions} from './api/fetchMissions';
+// jest.mock('./api/fetchMissions');
+
+test('app renders without errors', ()=> {
+    render(<App/>);
+});
+
+
+
+// test('Renders mission data when button is clicked', async ()=> {
+//     mockFetchMissions.mockResolvedValueOnce({
+//         data:[
+//             {mission_name:"Mission 1", mission_id:"mission 1"},
+//             {mission_name:"Mission 2", mission_id:"mission 2"}
+//         ]
+//     });
+
+//     //Arrange: render app
+//     render(<App />);
+
+//     //Act: get data from api? and push button
+//     const button = screen.getByRole('button');
+//     userEvent.click(button);
+    
+//     await waitFor(()=>{
+//         //Assert: We should get the same amount of missions as our api returned.
+//         expect(screen.getAllByTestId('mission')).toHaveLength(2);
+//     });
+// });
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Episodes from "./Episodes";
+
+
+
+// const missions = [
+//     {
+//         mission_name: "Thaicom",
+//         mission_id: "9D1B7E0"
+//     },
+//     {
+//         mission_name: "Telstar",
+//         mission_id: "F4F83DE"
+//     }
+// ];
+
+test('episodes renders without errors', ()=>{
+    render(<Episodes />);
+});
+
+// test('renders change in missions list correctly', ()=>{
+//     const { rerender } = render(<MissionsList missions={[]}/>);
+
+//     let missionObjects = screen.queryAllByTestId('mission');
+//     expect(missionObjects).toStrictEqual([]);
+//     expect(missionObjects).toHaveLength(0);
+
+//     rerender(<MissionsList missions={missions}/>);
+    
+//     missionObjects = screen.queryAllByTestId('mission');
+//     expect(missionObjects).toHaveLength(2);
+// });
