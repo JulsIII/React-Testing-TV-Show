caef8b20241abc47c8da31451b6d65480d82aa40
Your Name <you@example.com>
2021-01-27 22:09:20 -0600

    2b - Episodes Updating

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -7,7 +7,10 @@ import Episodes from "./Episodes";
 test('1st render, no epds displayed', ()=>{
    render(<Episodes episodes={[]} />);
 
-   const episode = screen.getByText(/episodes/i);
-   expect(episode).not.toBeInTheDocument();
+    // throw new Error("it is broken");
+
+   const episodes = screen.queryAllByText(/episodes/i);
+    // console.log(episodes);
+   expect(episodes).toHaveLength(0);
 
 });
